1687f9f1123d6239e78774347d2aaee0d317d710
Souvik Roy <souvikroy@Souviks-MacBook-Air.local>
2025-03-12 00:48:53 +0530

    learned about map filter reduce

--- Problems.js
+++ Problems.js
@@ -25,17 +25,71 @@ const users = [
 
 // Filter Users by Last Name   
 
-let output = {}
+// let output = {}
 
-const newArr= users.map((item)=>{
-  return item.lastName.startsWith('R')
-})
-console.log(newArr);
+// const newArr= []
+
+// users.forEach((item)=>{
+//   if(item.lastName.startsWith('R')){
+//     newArr.push(item)
+//   }
+// })
+
+// console.log(newArr);
+
 
 
 // Group Users by Age
+
+
+
 // Find the Oldest User
+
+// const oldUser = users.reduce((accumulator,user)=>{
+//   if(accumulator < user.age){
+//     accumulator = user.age
+//   }
+//   return accumulator
+// },0)
+
+//  users.forEach((item)=>{
+//   if(item.age === oldUser){
+//     // console.log(item);
+//     console.log(`${item.firstName} ${item.lastName}'s age is ${item.age}`);
+//   }
+// })
+
+
 // Calculate the Average Age
+
+// const sumOfAge = users.reduce((accumulator,user,)=>{
+//     accumulator = accumulator + user.age
+//     return accumulator
+// },0)
+
+// console.log(sumOfAge/users.length);
+
+
 //  Extract Full Names
+
+// users.forEach((item)=>{
+//   console.log(`${item.firstName} ${item.lastName}`);
+// })
+const newArr = users.map((item)=> `${item.firstName} ${item.lastName}`)
+console.log(newArr);
+
+
 // Find Users with Age Greater Than 30
+
+const ageGreaterThan30 = users.filter((item)=>{
+   return item.age > 30
+})
+
+console.log(ageGreaterThan30);
+
+
 // Sort Users by Age
+
+
+//Learn about objects
+
